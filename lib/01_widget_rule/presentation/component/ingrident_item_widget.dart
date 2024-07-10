3ba7732213a1d3195fc1e70cde6d7702c4d3e02c
lcw <skcjddn@gmail.com>
2024-07-10 23:45:46 +0900

    feat: add ingrident item widget

---
+++ lib/01_widget_rule/presentation/component/ingrident_item_widget.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import '../../data/model/ingrident.dart';
+
+class IngridentItem extends StatelessWidget {
+  final Ingrident ingrident;
+
+  const IngridentItem({super.key, required this.ingrident});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Row(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            child: Image.network(
+              ingrident.imageUrl,
+              width: 40,
+              height: 40,
+            ),
+          ),
+          const SizedBox(width: 10),
+          Expanded(
+            child: Text(
+              ingrident.name,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+          ),
+          Text(
+            '${ingrident.weight}g',
+            style: const TextStyle(
+              color: Colors.grey,
+              fontSize: 16,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

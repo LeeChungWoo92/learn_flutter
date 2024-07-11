8ca56bd4a9adf1b46454208525dd95777110b175
lcw <skcjddn@gmail.com>
2024-07-12 00:29:23 +0900

    add: ColorStyles

---
+++ lib/03_food_recipe_app/ui/color_styles.dart
@@ -0,0 +1,26 @@
+import 'dart:ui';
+
+abstract class ColorStyles {
+  static const Color blackColor = Color(0xFF000000);
+  static const Color gray1Color = Color(0xFF484848);
+  static const Color gray2Color = Color(0xFF797979);
+  static const Color gray3Color = Color(0xFFA9A9A9);
+  static const Color gray4Color = Color(0xFFD9D9D9);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+
+  static const Color primary100Color = Color(0xFF129575);
+  static const Color primary80Color = Color(0xFF71B1A1);
+  static const Color primary60Color = Color(0xFFAFD3CA);
+  static const Color primary40Color = Color(0xFFDBEBE7);
+  static const Color primary20Color = Color(0xFFF6FAF9);
+
+  static const Color secondary100Color = Color(0xFFFF9C00);
+  static const Color secondary80Color = Color(0xFFFFA61A);
+  static const Color secondary60Color = Color(0xFFFFBA4D);
+  static const Color secondary40Color = Color(0xFFFFCE80);
+  static const Color secondary20Color = Color(0xFFFFE1B3);
+
+  static const Color ratingColor = Color(0xFFFFAD30);
+  static const Color warningColor = Color(0xFFFD3654);
+  static const Color successColor = Color(0xFF31B057);
+}

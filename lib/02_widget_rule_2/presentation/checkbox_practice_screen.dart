d7ed4f4735a6ecf2875ddecac867817249c337ca
lcw <skcjddn@gmail.com>
2024-07-12 01:19:27 +0900

    add: practice file

---
+++ lib/02_widget_rule_2/presentation/checkbox_practice_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CheckboxPracticeScreen extends StatefulWidget {
+  const CheckboxPracticeScreen({super.key});
+
+  @override
+  State<CheckboxPracticeScreen> createState() => _CheckboxPracticeScreenState();
+}
+
+class _CheckboxPracticeScreenState extends State<CheckboxPracticeScreen> {
+  //final List<bool> _values = List.generate(10, (index) => false);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: [
+        ],
+      )),
+    );
+  }
+}

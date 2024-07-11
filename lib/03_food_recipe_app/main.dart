45adf3d124cf3247b926e3baa5c086e49c1a8a50
lcw <skcjddn@gmail.com>
2024-07-12 01:21:45 +0900

    add: food_recipe main

---
+++ lib/03_food_recipe_app/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:learn_flutter/03_food_recipe_app/presentaion/sign_in/sign_in_screen.dart';
+
+void main() {
+  runApp(const MainScreen());
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: SignInScreen(),
+      ),
+    );
+  }
+}

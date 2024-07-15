ddc465fc575d60aec1b9223498192a7a0258c2d2
lcw <skcjddn@gmail.com>
2024-07-16 01:18:19 +0900

    add: recipe model

---
+++ lib/04_recipe/data/model/recipe.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'recipe.freezed.dart';
+
+part 'recipe.g.dart';
+
+@freezed
+class Recipe with _$Recipe {
+  const factory Recipe({
+    required String imageUrl,
+    required String foodName,
+    required String chefName,
+    required double grade,
+    required String cookingTime,
+  }) = _Recipe;
+
+  factory Recipe.fromJson(Map<String, Object?> json) => _$RecipeFromJson(json);
+}

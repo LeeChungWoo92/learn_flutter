f41c9edf08954588a53b3dac9d555f4430aca624
lcw <skcjddn@gmail.com>
2024-07-17 14:31:52 +0900

    refactor: repositoryImpl to repository

--- lib/04_recipe/presentation/saved_recipe/saved_recipe_screen.dart
+++ lib/04_recipe/presentation/saved_recipe/saved_recipe_screen.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:learn_flutter/04_recipe/core/result.dart';
 import 'package:learn_flutter/04_recipe/data/model/recipe.dart';
+import 'package:learn_flutter/04_recipe/data/repository/recipe_repository.dart';
 import 'package:learn_flutter/04_recipe/data/repository/recipe_repository_impl.dart';
 import 'package:learn_flutter/04_recipe/presentation/saved_recipe/component/recipe_card_widget.dart';
 
 import '../../../03_food_recipe_app/ui/fonts.dart';
 
 class SavedRecipeScreen extends StatelessWidget {
-  final RecipeRepositoryImpl recipeRepositoryImpl;
+  final RecipeRepository recipeRepositoryImpl;
 
   const SavedRecipeScreen({
     super.key,

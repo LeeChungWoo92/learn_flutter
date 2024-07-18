1cecb84ac7c8f0ce865f79fbee6e61fce1df5113
lcw <skcjddn@gmail.com>
2024-07-19 03:18:48 +0900

    add: input_field_search widget

---
+++ lib/03_food_recipe_app/presentaion/component/input_field_search.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+import '../../ui/color_styles.dart';
+import '../../ui/fonts.dart';
+
+class InputFieldSearch extends StatelessWidget {
+  final bool isReadOnly;
+  final Function(String) onSearch;
+
+  const InputFieldSearch({
+    super.key,
+    this.isReadOnly = false,
+    required this.onSearch,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final contentEditController = TextEditingController();
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: ColorStyles.gray4Color),
+                ),
+                child: TextField(
+                  controller: contentEditController,
+                  readOnly: isReadOnly,
+                  decoration: InputDecoration(
+                    prefixIcon: Image.asset('assets/search_normal.png'),
+                    prefixIconColor: ColorStyles.gray4Color,
+                    hintText: 'Search recipe',
+                    hintStyle: Fonts.smallerTextRegular.copyWith(
+                      color: ColorStyles.gray4Color,
+                    ),
+                    border: InputBorder.none,
+                    contentPadding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 10,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(width: 20),
+            GestureDetector(
+              onTap: () {
+                FocusScope.of(context).unfocus();
+                onSearch(contentEditController.text);
+              },
+              child: Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: ColorStyles.primary100Color,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Image.asset(
+                  'assets/setting.png',
+                  width: 20,
+                  height: 20,
+                  color: ColorStyles.whiteColor,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}

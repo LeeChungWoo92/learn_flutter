6a8a0bfe61f3ab72d409f009455e1607105d4a43
lcw <skcjddn@gmail.com>
2024-07-19 11:08:39 +0900

    add: router screen

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,6 +1,9 @@
 import 'package:go_router/go_router.dart';
-import 'package:learn_flutter/04_recipe/data/repository/recipe_repository.dart';
-import 'package:learn_flutter/04_recipe/presentation/saved_recipe/component/saved_recipes_view_model.dart';
+import 'package:learn_flutter/03_food_recipe_app/presentaion/sign_up/sign_up_screen.dart';
+import 'package:learn_flutter/03_food_recipe_app/presentaion/splash/splash_screen.dart';
+import 'package:learn_flutter/04_recipe/presentation/home/search_recipes_screen.dart';
+import 'package:learn_flutter/04_recipe/presentation/home/search_recipes_view_model.dart';
+import 'package:learn_flutter/04_recipe/presentation/saved_recipe/saved_recipes_view_model.dart';
 
 import '../04_recipe/data/datasource/recipe_datasource_impl.dart';
 import '../04_recipe/data/model/recipe.dart';
@@ -13,7 +16,7 @@ final _dataource = RecipeDatasourceImpl();
 final _repository = RecipeRepositoryImpl(_dataource);
 
 final router = GoRouter(
-  initialLocation: '/home',
+  initialLocation: '/sign_up',
   routes: [
     GoRoute(
       path: '/home',
@@ -34,6 +37,25 @@ final router = GoRouter(
       builder: (context, state) {
         final recipe = state.extra as Recipe;
         return SavedRecipeDetailScreen(recipe: recipe);
+      },
+    ),
+    GoRoute(
+      path: '/splash',
+      builder: (context, state) {
+        return const SplashScreen();
+      },
+    ),
+    GoRoute(
+      path: '/sign_up',
+      builder: (context, state) {
+        return const SignUpScreen();
+      },
+    ),
+    GoRoute(
+      path: '/search_recipes',
+      builder: (context, state) {
+        final viewModel = SearchRecipesViewModel(_repository);
+        return SearchRecipesScreen(viewModel: viewModel);
       },
     ),
   ],

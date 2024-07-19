73593fac2183ff373d806ed25c67f7f4f0b52d02
lcw <skcjddn@gmail.com>
2024-07-19 12:00:22 +0900

    rename: food name ko to eng

--- lib/04_recipe/data/datasource/recipe_datasource_impl.dart
+++ lib/04_recipe/data/datasource/recipe_datasource_impl.dart
@@ -8,7 +8,7 @@ class RecipeDatasourceImpl implements RecipeDatasource {
       const Recipe(
         imageUrl:
             'https://cdn.pixabay.com/photo/2019/11/23/15/25/korea-food-4647402_1280.jpg',
-        foodName: '삼겹살',
+        foodName: 'Samgyeobsal',
         chefName: '이청우',
         grade: 4.5,
         cookingTime: '25',
@@ -16,7 +16,7 @@ class RecipeDatasourceImpl implements RecipeDatasource {
       const Recipe(
         imageUrl:
             'https://cdn.pixabay.com/photo/2020/04/26/22/32/black-bean-noodles-5097423_1280.jpg',
-        foodName: '짜장면',
+        foodName: 'Jajangmyeon',
         chefName: '이연복',
         grade: 5.0,
         cookingTime: '20',
@@ -24,7 +24,7 @@ class RecipeDatasourceImpl implements RecipeDatasource {
       const Recipe(
         imageUrl:
             'https://cdn.pixabay.com/photo/2020/10/11/05/06/chicken-5644762_1280.jpg',
-        foodName: '치킨',
+        foodName: 'Chicken',
         chefName: '백종원',
         grade: 4.5,
         cookingTime: '35',
@@ -32,7 +32,7 @@ class RecipeDatasourceImpl implements RecipeDatasource {
       const Recipe(
         imageUrl:
             'https://cdn.pixabay.com/photo/2018/12/03/01/04/spicy-seafood-3852529_1280.jpg',
-        foodName: '짬뽕',
+        foodName: 'Jjambbong',
         chefName: '이청우',
         grade: 4.0,
         cookingTime: '15',
@@ -40,7 +40,7 @@ class RecipeDatasourceImpl implements RecipeDatasource {
       const Recipe(
         imageUrl:
             'https://cdn.pixabay.com/photo/2016/10/13/19/15/bibimbap-1738580_1280.jpg',
-        foodName: '비빔밥',
+        foodName: 'Bibimbap',
         chefName: 'Hong gil dong',
         grade: 4.0,
         cookingTime: '15',
@@ -48,7 +48,7 @@ class RecipeDatasourceImpl implements RecipeDatasource {
       const Recipe(
         imageUrl:
             'https://cdn.pixabay.com/photo/2024/04/23/09/32/ai-generated-8714517_1280.jpg',
-        foodName: '피자',
+        foodName: 'Pizza',
         chefName: '도민호',
         grade: 5.0,
         cookingTime: '30',
@@ -56,7 +56,7 @@ class RecipeDatasourceImpl implements RecipeDatasource {
       const Recipe(
         imageUrl:
         'https://cdn.pixabay.com/photo/2024/05/02/17/47/sushi-8735286_1280.jpg',
-        foodName: '초밥',
+        foodName: 'Sushi',
         chefName: '오노 지로',
         grade: 5.0,
         cookingTime: '60',

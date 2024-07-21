81a20dedf999720b6c9e2d9fe6cb99aa1b673f23
lcw <skcjddn@gmail.com>
2024-07-21 20:18:54 +0900

    refactor: use widget

--- lib/04_recipe/presentation/saved_recipe/saved_recipe_detail_screen.dart
+++ lib/04_recipe/presentation/saved_recipe/saved_recipe_detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:learn_flutter/04_recipe/data/model/recipe.dart';
 import 'package:learn_flutter/04_recipe/presentation/component/profile_widget.dart';
+import 'package:learn_flutter/04_recipe/presentation/saved_recipe/component/recipe_card_widget.dart';
 
 import '../../../01_widget_rule/data/model/profile.dart';
 
@@ -30,137 +31,43 @@ class SavedRecipeDetailScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(),
-      body: Hero(
-        tag: recipe.imageUrl,
-        child: SingleChildScrollView(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Card(
-                color: Colors.black,
-                margin: const EdgeInsets.only(left: 30, right: 30),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(6.0),
-                  child: Stack(
-                    children: [
-                      Image.network(
-                        recipe.imageUrl,
-                        width: double.infinity,
-                        height: 160,
-                        fit: BoxFit.cover,
-                      ),
-                      Container(
-                        width: double.infinity,
-                        height: 160,
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter,
-                            colors: [
-                              Colors.black.withOpacity(0.1),
-                              Colors.black.withOpacity(0.7),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        top: 8,
-                        right: 8,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white.withOpacity(0.8),
-                            borderRadius: BorderRadius.circular(16),
-                          ),
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 8,
-                            vertical: 4,
-                          ),
-                          child: Row(
-                            children: [
-                              const Icon(
-                                Icons.star,
-                                color: Colors.amber,
-                                size: 16,
-                              ),
-                              const SizedBox(width: 4),
-                              Text(recipe.grade.toString(),
-                                  style: const TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                  )),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        bottom: 12,
-                        right: 8,
-                        child: Row(
-                          children: [
-                            const Icon(
-                              Icons.timer_outlined,
-                              color: Colors.white,
-                            ),
-                            const SizedBox(width: 10),
-                            Text(
-                              '${recipe.cookingTime} min',
-                              style: const TextStyle(
-                                fontSize: 14,
-                                color: Colors.white,
-                              ),
-                            ),
-                            const SizedBox(width: 10),
-                            Container(
-                                width: 30,
-                                height: 30,
-                                decoration: const BoxDecoration(
-                                    color: Colors.white,
-                                    shape: BoxShape.circle),
-                                child: const ClipOval(
-                                    child: Icon(
-                                  Icons.bookmark_border,
-                                  color: Colors.green,
-                                )))
-                          ],
-                        ),
-                      ),
-                    ],
+              Hero(
+                  tag: recipe.imageUrl,
+                  child: RecipeCardWidget(recipe: recipe)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    recipe.foodName,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 40, right: 40, top: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      recipe.foodName,
-                      style: const TextStyle(
-                        color: Colors.black,
+                  const Material(
+                    child: Text(
+                      '(13k Reviews)',
+                      style: TextStyle(
+                        color: Colors.grey,
                         fontSize: 16,
-                        fontWeight: FontWeight.bold,
+                        fontWeight: FontWeight.normal,
                       ),
                     ),
-                    const Material(
-                      child: Text(
-                        '(13k Reviews)',
-                        style: TextStyle(
-                          color: Colors.grey,
-                          fontSize: 16,
-                          fontWeight: FontWeight.normal,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
               const SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 30),
-                child: ProfileWidget(
-                    profile: _profile,
-                    onFollowTap: _toggleFollow,
-                    isFollow: _isFollow),
-              )
+              ProfileWidget(
+                  profile: _profile,
+                  onFollowTap: _toggleFollow,
+                  isFollow: _isFollow)
             ],
           ),
         ),

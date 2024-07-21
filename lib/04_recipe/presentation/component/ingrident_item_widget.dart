754ab1ffcf3bfc3eccc63c910374732756df2db6
lcw <skcjddn@gmail.com>
2024-07-22 00:08:32 +0900

    refactor: ingrident_item_widget setting

--- lib/04_recipe/presentation/component/ingrident_item_widget.dart
+++ lib/04_recipe/presentation/component/ingrident_item_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learn_flutter/03_food_recipe_app/ui/color_styles.dart';
 
 import '../../../01_widget_rule/data/model/ingrident.dart';
 
@@ -13,9 +14,9 @@ class IngridentItemWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.all(15.0),
       decoration: BoxDecoration(
-        color: Colors.grey[200],
+        color: ColorStyles.gray4Color.withOpacity(0.5),
         borderRadius: BorderRadius.circular(10.0),
       ),
       child: Row(
@@ -32,7 +33,7 @@ class IngridentItemWidget extends StatelessWidget {
               height: 40,
             ),
           ),
-          const SizedBox(width: 10),
+          const SizedBox(width: 16),
           Expanded(
             child: Text(
               ingrident.name,

0b0f2525df05c68bad1e2016b4c72b6e0af9c41e
lcw <skcjddn@gmail.com>
2024-07-22 15:02:30 +0900

    rename: IngridentViewModel to  SavedRecipeDetailViewModel

--- lib/04_recipe/presentation/saved_recipe/saved_recipe_detail_screen.dart
+++ lib/04_recipe/presentation/saved_recipe/saved_recipe_detail_screen.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:learn_flutter/03_food_recipe_app/ui/color_styles.dart';
+import 'package:learn_flutter/03_food_recipe_app/ui/fonts.dart';
+import 'package:learn_flutter/04_recipe/core/change_notifier_provider.dart';
 import 'package:learn_flutter/04_recipe/data/model/recipe.dart';
+import 'package:learn_flutter/04_recipe/presentation/component/ingrident_item_widget.dart';
+import 'package:learn_flutter/04_recipe/presentation/component/procedure_item_widget.dart';
 import 'package:learn_flutter/04_recipe/presentation/component/profile_widget.dart';
 import 'package:learn_flutter/04_recipe/presentation/saved_recipe/component/recipe_card_widget.dart';
+import 'package:learn_flutter/04_recipe/presentation/saved_recipe/saved_recipe_detail_view_model.dart';
 
 import '../../../01_widget_rule/data/model/profile.dart';
 
-class SavedRecipeDetailScreen extends StatelessWidget {
+class SavedRecipeDetailScreen extends StatefulWidget {
   final Recipe recipe;
 
   const SavedRecipeDetailScreen({
@@ -14,21 +20,28 @@ class SavedRecipeDetailScreen extends StatelessWidget {
   });
 
   @override
+  State<SavedRecipeDetailScreen> createState() =>
+      _SavedRecipeDetailScreenState();
+}
+
+class _SavedRecipeDetailScreenState extends State<SavedRecipeDetailScreen> {
+  static const profile = Profile(
+    imageUrl:
+        'https://cdn.pixabay.com/photo/2020/06/25/16/25/micky-mouse-5340128_1280.jpg',
+    name: '미키마우스',
+    locationName: 'Lagos,Nigeria',
+  );
+
+  bool isFollow = false;
+
+  void toggleFollow(String message) {
+    setState(() {
+      isFollow = !isFollow;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final _profile = const Profile(
-      imageUrl:
-          'https://cdn.pixabay.com/photo/2020/06/25/16/25/micky-mouse-5340128_1280.jpg',
-      name: '미키마우스',
-      locationName: 'Lagos,Nigeria',
-    );
-
-    bool _isFollow = false;
-
-    void _toggleFollow(String message) {
-      _isFollow = !_isFollow;
-      print(message);
-    }
-
     return Scaffold(
       appBar: AppBar(),
       body: SingleChildScrollView(
@@ -38,13 +51,13 @@ class SavedRecipeDetailScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Hero(
-                  tag: recipe.imageUrl,
-                  child: RecipeCardWidget(recipe: recipe)),
+                  tag: widget.recipe.imageUrl,
+                  child: RecipeCardWidget(recipe: widget.recipe)),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(
-                    recipe.foodName,
+                    widget.recipe.foodName,
                     style: const TextStyle(
                       color: Colors.black,
                       fontSize: 16,
@@ -65,13 +78,177 @@ class SavedRecipeDetailScreen extends StatelessWidget {
               ),
               const SizedBox(height: 10),
               ProfileWidget(
-                  profile: _profile,
-                  onFollowTap: _toggleFollow,
-                  isFollow: _isFollow)
+                  profile: profile,
+                  onFollowTap: toggleFollow,
+                  isFollow: isFollow),
+              const SizedBox(height: 8),
+              tabBar(),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget tabBar() {
+    return DefaultTabController(
+      length: 2,
+      child: Column(
+        children: [
+          SizedBox(
+            height: 58,
+            child: TabBar(
+              labelStyle: const TextStyle(
+                fontSize: 11,
+                fontWeight: FontWeight.bold,
+                height: 17 / 11,
+              ),
+              unselectedLabelStyle: const TextStyle(
+                fontSize: 11,
+                fontWeight: FontWeight.bold,
+                height: 17 / 11,
+              ),
+              tabs: const [
+                Tab(
+                  text: 'Ingredient',
+                  height: 33,
+                ),
+                Tab(
+                  text: 'Procedure',
+                  height: 33,
+                ),
+              ],
+
+              dividerColor: Colors.transparent,
+              indicator: BoxDecoration(
+                color: ColorStyles.primary100Color,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              indicatorSize: TabBarIndicatorSize.tab,
+              indicatorPadding: const EdgeInsets.symmetric(vertical: 8),
+              labelColor: ColorStyles.whiteColor,
+              // 선택된 탭의 텍스트 색상
+              unselectedLabelColor: ColorStyles.primary80Color,
+            ),
+          ),
+          const SizedBox(height: 22),
+          SizedBox(
+            height: 450,
+            child: TabBarView(
+              children: [
+                ingredientList(),
+                proceduretList(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget ingredientList() {
+    final viewModel =
+        ChangeNotifierProvider.of<SavedRecipeDetailViewModel>(context).value;
+
+    return ListenableBuilder(
+      listenable: viewModel,
+      builder: (BuildContext context, Widget? child) {
+        final ingridents = viewModel.ingrident;
+        return Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Image.asset('assets/ic_food.png'),
+                    const SizedBox(width: 5),
+                    Text('1 serve',
+                        style: Fonts.smallerTextRegular.copyWith(
+                          color: ColorStyles.gray3Color,
+                        )),
+                  ],
+                ),
+                Text('${viewModel.ingrident.length} Items',
+                    style: Fonts.smallerTextRegular.copyWith(
+                      color: ColorStyles.gray3Color,
+                    )),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: ingridents.length,
+                itemBuilder: (context, index) {
+                  final ingrident = ingridents[index];
+                  if (viewModel.isLoading) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: IngridentItemWidget(ingrident: ingrident),
+                  );
+                },
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget proceduretList() {
+    final viewModel =
+        ChangeNotifierProvider.of<SavedRecipeDetailViewModel>(context).value;
+
+    return ListenableBuilder(
+      listenable: viewModel,
+      builder: (BuildContext context, Widget? child) {
+        final procedures = viewModel.procedure;
+        return Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Image.asset('assets/ic_food.png'),
+                    const SizedBox(width: 5),
+                    Text('1 serve',
+                        style: Fonts.smallerTextRegular.copyWith(
+                          color: ColorStyles.gray3Color,
+                        )),
+                  ],
+                ),
+                Text('${viewModel.procedure.length} Steps',
+                    style: Fonts.smallerTextRegular.copyWith(
+                      color: ColorStyles.gray3Color,
+                    )),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: procedures.length,
+                itemBuilder: (context, index) {
+                  final procedure = procedures[index];
+                  if (viewModel.isLoading) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: ProcedureItemWidget(procedure: procedure),
+                  );
+                },
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
--- lib/04_recipe/presentation/saved_recipe/saved_recipe_detail_view_model.dart
+++ lib/04_recipe/presentation/saved_recipe/saved_recipe_detail_view_model.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:learn_flutter/01_widget_rule/data/model/ingrident.dart';
 import 'package:learn_flutter/04_recipe/core/result.dart';
+import 'package:learn_flutter/04_recipe/data/model/procedure.dart';
 import 'package:learn_flutter/04_recipe/data/repository/ingrident_repository.dart';
+import 'package:learn_flutter/04_recipe/data/repository/procedure_repository.dart';
 
-class IngridentViewModel with ChangeNotifier {
+class SavedRecipeDetailViewModel with ChangeNotifier {
   final IngridentRepository _ingridentRepository;
+  final ProcedureRepository _procedureRepository;
 
-  IngridentViewModel(this._ingridentRepository) {
+  SavedRecipeDetailViewModel(
+    this._ingridentRepository,
+    this._procedureRepository,
+  ) {
     getIngridents();
+    getProcedures();
   }
 
   List<Ingrident> _ingrident = [];
 
   List<Ingrident> get ingrident => List.unmodifiable(_ingrident);
+
+  List<Procedure> _procedure = [];
+
+  List<Procedure> get procedure => List.unmodifiable(_procedure);
 
   bool _isLoading = false;
 
@@ -29,8 +40,21 @@ class IngridentViewModel with ChangeNotifier {
     switch (data) {
       case Success<List<Ingrident>>():
         _ingrident = data.data;
-        print('_ingrident $ingrident');
       case Error<List<Ingrident>>():
+        _errorMessage = '에러발생';
+    }
+
+    notifyListeners();
+  }
+
+  void getProcedures() async {
+    _isLoading = true;
+    final data = await _procedureRepository.getProcedures();
+    _isLoading = false;
+    switch (data) {
+      case Success<List<Procedure>>():
+        _procedure = data.data;
+      case Error<List<Procedure>>():
         _errorMessage = '에러발생';
     }
 

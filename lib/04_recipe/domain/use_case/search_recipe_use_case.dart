41e60ceede941525aa708bd84773156f8817fd17
lcw <skcjddn@gmail.com>
2024-07-26 19:03:25 +0900

    add: search_recipe_use_case.dart

---
+++ lib/04_recipe/domain/use_case/search_recipe_use_case.dart
@@ -0,0 +1,23 @@
+import 'package:learn_flutter/04_recipe/core/result.dart';
+import 'package:learn_flutter/04_recipe/domain/model/recipe.dart';
+import 'package:learn_flutter/04_recipe/domain/use_case/get_recipe_use_case.dart';
+
+class SearchRecipeUseCase {
+  final GetRecipeUseCase _getRecipeUseCase;
+
+  SearchRecipeUseCase(this._getRecipeUseCase);
+
+  Future<Result<List<Recipe>>> execute(String foodName) async {
+    final results = await _getRecipeUseCase.execute();
+    switch (results) {
+      case Success<List<Recipe>>():
+        final searchResults = results.data
+            .where((recipe) =>
+                recipe.foodName.toLowerCase().contains(foodName.toLowerCase()))
+            .toList();
+        return Result.success(searchResults);
+      case Error<List<Recipe>>():
+        return const Result.error('에러발생');
+    }
+  }
+}

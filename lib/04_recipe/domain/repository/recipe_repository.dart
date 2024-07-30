c3e647c1bcbaf44002ebabe7f7a44cb94c4ed0f8
lcw <skcjddn@gmail.com>
2024-07-30 19:49:36 +0900

    remove: not use fuction

--- lib/04_recipe/domain/repository/recipe_repository.dart
+++ lib/04_recipe/domain/repository/recipe_repository.dart
@@ -3,6 +3,4 @@ import 'package:learn_flutter/04_recipe/domain/model/recipe.dart';
 
 abstract interface class RecipeRepository {
   Future<Result<List<Recipe>>> getRecipes();
-
-  Future<Result<List<Recipe>>> getSearchRecipes(String foodName);
 }

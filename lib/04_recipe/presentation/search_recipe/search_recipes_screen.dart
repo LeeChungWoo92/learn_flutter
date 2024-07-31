35734d6153fbfb6d42e3248ab4ce5502ef77c527
lcw <skcjddn@gmail.com>
2024-07-31 18:14:46 +0900

    refactor: use provider code

--- lib/04_recipe/presentation/search_recipe/search_recipes_screen.dart
+++ lib/04_recipe/presentation/search_recipe/search_recipes_screen.dart
@@ -2,16 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:learn_flutter/04_recipe/presentation/component/input_field_search.dart';
 import 'package:learn_flutter/04_recipe/presentation/search_recipe/search_recipes_view_model.dart';
 import 'package:learn_flutter/04_recipe/presentation/saved_recipe/component/recipe_card_widget.dart';
+import 'package:provider/provider.dart';
 
 import '../../../03_food_recipe_app/ui/fonts.dart';
 
 class SearchRecipesScreen extends StatelessWidget {
-  final SearchRecipesViewModel viewModel;
-
-  const SearchRecipesScreen({super.key, required this.viewModel});
+  const SearchRecipesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final viewModel = context.watch<SearchRecipesViewModel>();
     return Scaffold(
       appBar: AppBar(
         title: Text('Search recipes', style: Fonts.mediumTextBold),
@@ -31,7 +31,7 @@ class SearchRecipesScreen extends StatelessWidget {
             Text('Search Result', style: Fonts.normalTextBold),
             const SizedBox(height: 20),
             Expanded(
-              child: recipeList(),
+              child: recipeList(context),
             ),
           ],
         ),
@@ -39,42 +39,39 @@ class SearchRecipesScreen extends StatelessWidget {
     );
   }
 
-  Widget recipeList() {
-    return ListenableBuilder(
-      listenable: viewModel,
-      builder: (BuildContext context, Widget? child) {
-        final recipes = viewModel.state.recipe;
+  Widget recipeList(BuildContext context) {
+    final viewModel = context.watch<SearchRecipesViewModel>();
 
-        if (recipes.isEmpty) {
-          return const Center(
-            child: Text(
-              '검색 결과가 없습니다.',
-              style: TextStyle(
-                color: Colors.black,
-              ),
-            ),
+    final recipes = viewModel.state.recipe;
+
+    if (recipes.isEmpty) {
+      return const Center(
+        child: Text(
+          '검색 결과가 없습니다.',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+      );
+    }
+
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        mainAxisSpacing: 15.0,
+      ),
+      itemCount: recipes.length,
+      itemBuilder: (context, index) {
+        final recipe = recipes[index];
+        if (viewModel.state.isLoading) {
+          const Center(
+            child: CircularProgressIndicator(),
           );
         }
 
-        return GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            mainAxisSpacing: 15.0,
-          ),
-          itemCount: recipes.length,
-          itemBuilder: (context, index) {
-            final recipe = recipes[index];
-            if (viewModel.state.isLoading) {
-              const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-
-            return RecipeCardWidget(
-              recipe: recipe,
-              isVisibleDetailUi: false,
-            );
-          },
+        return RecipeCardWidget(
+          recipe: recipe,
+          isVisibleDetailUi: false,
         );
       },
     );
